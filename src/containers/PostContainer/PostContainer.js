ed34b95f4f174a260a6d74b3b448a62dafea9a0b
Jicheol Oh <jicheol@gmail.com>
2019-06-26 11:36:45 -0700

    5.3 컴포넌트에서 API 사용하기

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 import { PostWrapper, Navigator, Post } from '../../components';
 //import { PostWrapper } from 'components/'
+import * as service from '../../services/post';
 
 // 함수형 컴포넌트가 아닌 클래스 형식으로 컴포넌트를 선언했습니다. 그 이유는 나중에 여기에 state 를 추가할것이기 때문이죠!
 class PostContainer extends Component {
+
+    fetchPostInfo = async (postId) => {
+        const post = await service.getPost(postId);
+        console.log(post);
+        const comments = await service.getComments(postId);
+        console.log(comments);
+    }
+
     render() {
         return (
             <PostWrapper>
