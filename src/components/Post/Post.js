2be4a205c7c9928b3d7b933dc42e7105e52acb9d
Jicheol Oh <jicheol@gmail.com>
2019-06-26 11:10:05 -0700

    4.3 CommentList 컴포넌트 만들기 - 컴포넌트 선언 - 컴포넌트 스타일링 - 컴포넌트 인덱스 설정 - 컴포넌트 사용

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Post.css';
+import { CommentList } from '../';
 
 // 포스트의 내용과, 덧글들을 담을 Post 컴포넌트를 만들어봅시다.
 const Post = () => (
@@ -8,6 +9,7 @@ const Post = () => (
         <p>
         Body
         </p>
+        <CommentList/>
     </div>
 
 );
