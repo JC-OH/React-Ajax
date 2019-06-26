1a0ea17198640468bc1fb591dde3f7531b06dbb2
Jicheol Oh <jicheol@gmail.com>
2019-06-26 10:36:53 -0700

    3.6 똑똑한 컴포넌트, PostContainer 만들기 - 컴포넌트 선언 - 컴포넌트 인덱스 설정 - 컴포넌트 불러오기

---
+++ src/containers/PostContainer/PostContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { PostWrapper } from '../../components';
+//import { PostWrapper } from 'components/'
+
+// 함수형 컴포넌트가 아닌 클래스 형식으로 컴포넌트를 선언했습니다. 그 이유는 나중에 여기에 state 를 추가할것이기 때문이죠!
+class PostContainer extends Component {
+    render() {
+        return (
+            <PostWrapper>
+                Hello, Post
+            </PostWrapper>
+        );
+    }
+}
+
+export default PostContainer;
