f2a385b884708c12257c04ec8236eb8201e0ec16
Jicheol Oh <jicheol@gmail.com>
2019-06-26 11:18:00 -0700

    4.4 Comment 컴포넌트 만들기 - 컴포넌트 선언 - 컴포넌트 스타일링 - 컴포넌트 인덱스 설정 - 컴포넌트 사용

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comment.css';
+
+//  CommentList 내부에 들어가는 컴포넌트이며, li 태그로 이뤄져있습니다.
+const Comment = () => {
+    return (
+        <li className="Comment">
+        <p>
+        <b>name</b> body
+        </p>
+        </li>
+    );
+};
+export default Comment;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,8 @@ import Header from './Header/Header';
 import PostWrapper from './PostWrapper/PostWrapper';
 import Navigator from './Navigator/Navigator';
 import Post from './Post/Post';
+
+import Comment from './Comment/Comment';
 import CommentList from './CommentList/CommentList';
 
 export {
@@ -9,5 +11,6 @@ export {
     PostWrapper,
     Navigator,
     Post,
+    Comment,
     CommentList
 }
